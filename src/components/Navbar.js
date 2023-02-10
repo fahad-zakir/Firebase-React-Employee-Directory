bdeeb14b87dcd9666d3cb97ba54bd75311695aa5
fahad-zakir <fahadzakir81@gmail.com>
2023-02-10 14:51:56 -0600

    added-search-form-css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../images/oj-icon.png'; 
+import SearchBar from './SearchBar';
 
 function Navbar () {
    return (
      <nav>
-       <img src={logo} className="nav--icon" alt="logo-icon" />
-       <h2 className="nav--logo_text">OJ-Employee-Directory</h2>
+       <img src={logo} className="nav--icon" alt="logo--icon" />
+       <h2 className="nav--logo_text">OJ Lifestyle Employee Directory</h2>
      </nav>
    );
 }
