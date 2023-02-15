0b0791e263197ebfe42ab2ac01101630a1df4606
fahad-zakir <fahadzakir81@gmail.com>
2023-02-15 12:42:11 -0600

    updated-form-css

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -8,44 +8,60 @@ function EmployeeForm({
   handleButtonClick,
 }) {
   return (
-    <form onSubmit={handleSubmit} className="row d-flex justify-content-center">
-      <div className="col-sm-12 custom-form">
-        <input
-          type="text"
-          onChange={handleChange}
-          value={employeeInfo.fullName}
-          name="fullName"
-          className="form-control"
-          placeholder="Full Name"
-          required
-        />
-        <input
-          type="text"
-          onChange={handleChange}
-          value={employeeInfo.jobTitle}
-          name="jobTitle"
-          className="form-control"
-          placeholder="Job Title"
-          required
-        />
-        <input
-          type="email"
-          onChange={handleChange}
-          value={employeeInfo.emailAddress}
-          name="emailAddress"
-          className="form-control"
-          placeholder="Email Address"
-          required
-        />
-        <input
-          type="text"
-          onChange={handleChange}
-          value={employeeInfo.phoneNumber}
-          name="phoneNumber"
-          className="form-control"
-          placeholder="Phone Number"
-          required
-        />
+    <form
+      onSubmit={handleSubmit}
+      className="form-container row d-flex justify-content-center"
+    >
+      <h3 id="form-signup" className="col-sm-12 d-flex justify-content-center">
+        Add Employee
+      </h3>
+      <div className="col-sm-12">
+      <label>Employee Name:</label>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={employeeInfo.fullName}
+        name="fullName"
+        className="form-control"
+        placeholder="Full Name"
+        required
+      />
+      </div>
+            <div className="col-sm-12">
+<label >Job Title</label>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={employeeInfo.jobTitle}
+        name="jobTitle"
+        className="form-control"
+        placeholder="Job Title"
+        required
+      />
+      </div>
+                  <div className="col-sm-12">
+<label >Email</label>
+      <input
+        type="email"
+        onChange={handleChange}
+        value={employeeInfo.emailAddress}
+        name="emailAddress"
+        className="form-control"
+        placeholder="Email Address"
+        required
+      />
+      </div>
+                  <div className="col-sm-12">
+<label >Phone Number</label>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={employeeInfo.phoneNumber}
+        name="phoneNumber"
+        className="form-control"
+        placeholder="Phone Number"
+        required
+      />
       </div>
       <div className="col-sm-12 d-flex justify-content-center form-buttons">
         <input
