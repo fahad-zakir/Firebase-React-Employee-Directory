b04e85c49fed8f04735e7448ad85fcf3aa7c86ee
fahad-zakir <fahadzakir81@gmail.com>
2023-02-26 14:56:39 -0600

    updated-css

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -21,7 +21,7 @@ function EmployeeList({
           width: isSmallScreen ? "80%" : "105%",
           marginTop: "5%",
           marginBottom: "3%",
-          marginLeft: isSmallScreen ? "2px" : "12px",
+          marginLeft: isSmallScreen ? "2px" : "2px",
         }}
         variant="solid"
         color="primary"
