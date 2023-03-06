c908154becda3df3ff7d8f50bdfe9e19206c9c97
fahad-zakir <fahadzakir81@gmail.com>
2023-03-05 22:49:52 -0600

    updated-env

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import logo from "../images/oj-icon.png";
+import AdminLogin from "./AdminLogin";
 
 function Navbar() {
   return (
     <div className="row nav-col">
-      <div className="col-sm-12">
+      <div className="col-sm-12 logo-row d-flex justify-content-between">
         <img src={logo} className="nav--icon" alt="logo--icon" />
+        <AdminLogin />
       </div>
+
       <div className="col-sm-12 d-flex justify-content-center">
         <h2 className="nav--logo_text">OJ Employee Directory</h2>
       </div>
--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -4,13 +4,20 @@ import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
+
+// #   apiKey: "AIzaSyCggz-IueBgpcGTsmPmJvF-CXjlh7JVnU8",
+// #   authDomain: "oj-employee-directory.firebaseapp.com",
+// #   projectId: "oj-employee-directory",
+// #   storageBucket: "oj-employee-directory.appspot.com",
+// #   messagingSenderId: "1052728689574",
+// #   appId: "1:1052728689574:web:b7023dd25baf7821340567",
+// #   measurementId: "G-180E7FRY35",
 
 const app = initializeApp(firebaseConfig);
 
