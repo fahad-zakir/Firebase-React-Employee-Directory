4aa73eea557391132492e31fce8e816f5718f418
fahad-zakir <fahadzakir81@gmail.com>
2023-03-09 13:38:16 -0600

    updated-css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import AdminLogin from "./AdminLogin";
 function Navbar() {
   return (
     <div className="row nav-col">
-      <div className="col-sm-12 logo-row d-flex">
+      {/* <div className="col-sm-12 logo-row d-flex">
         <AdminLogin />
-      </div>
+      </div> */}
       <div className="col-sm-12 d-flex justify-content-center">
         <h2 className="nav--logo_text">Employee Directory</h2>
       </div>
